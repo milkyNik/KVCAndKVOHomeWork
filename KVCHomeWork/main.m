b9dcd409c7ef651154909bf51b5dace9c3e7ee62
milkyNik <milniklaus@gmail.com>
2016-10-12 15:22:01 +0300

    Initial Commit

---
+++ KVCHomeWork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVCHomeWork
+//
+//  Created by iMac on 12.10.16.
+//  Copyright © 2016 hata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
